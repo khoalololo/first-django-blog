312903fdb4d1bd1b8b679d23a1e9a3afb3828bd1
khoalololo <minhkhoa278kd@gmail.com>
2025-06-22 20:07:53 +0700

    Feat: Implement custom GNU Terry Pratchett middleware

---
+++ blog/middleware.py
@@ -0,0 +1,10 @@
+# blog/middleware.py
+
+class ClacksMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['X-Clacks-Overhead'] = 'GNU Terry Pratchett'
+        return response
--- mysite/settings.py
+++ mysite/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'gnu_terry_pratchett.middleware.ClacksMiddleware',
+    "blog.middleware.ClacksMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
