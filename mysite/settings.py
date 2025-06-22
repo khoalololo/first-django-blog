2f79c4b4de53044048ec8c20b78594018ec290a1
khoalololo <minhkhoa278kd@gmail.com>
2025-06-22 19:27:39 +0700

    Fix: Corrected STATIC_ROOT and STATICFILES_DIRS conflict

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,8 +122,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
