433e878245a95ef7d4f654a0e10939fbbf6ec770
khoalololo <minhkhoa278kd@gmail.com>
2025-06-22 15:10:53 +0700

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,6 +111,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_CHARSET = 'utf-8'
+
+# This should generally be True for internationalization
+USE_I18N = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
